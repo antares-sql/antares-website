dba8d74e451afc7a55bc15804d69354d2a4c4620
Fabio Di Stasio <fabio286@gmail.com>
2022-07-11 16:41:18 +0200

    set router base path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: 'static',
+  router: {
+    base: '/antares-website/'
+  },
   vite: {
     plugins: [
       eslintPlugin()
