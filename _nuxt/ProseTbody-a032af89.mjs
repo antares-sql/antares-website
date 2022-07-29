e1af79f8c5370fa04e66613a05b8892d66db8bce
Fabio286 <Fabio286@users.noreply.github.com>
2022-07-29 07:33:46 +0000

    deploy: 14965b9b7976419a8800c1b6d5a7289bfcc52abe

---
+++ _nuxt/ProseTbody-a032af89.mjs
@@ -0,0 +1,1 @@
+import{e as r,o,f as t,r as s}from"./entry-1bf68c30.mjs";const a={};function n(e,c){return o(),t("tbody",null,[s(e.$slots,"default")])}var f=r(a,[["render",n]]);export{f as default};
