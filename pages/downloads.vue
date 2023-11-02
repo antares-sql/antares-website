e7a3294d3ef489d677d79fb98ce57d867004be98
Fabio Di Stasio <fabio286@gmail.com>
2023-11-02 09:51:57 +0100

    feat: flatpak badge on download page

--- pages/downloads.vue
+++ pages/downloads.vue
@@ -118,7 +118,7 @@
                 </tr>
               </tbody>
             </table>
-            <a class="m-auto block" href="//www.microsoft.com/store/apps/9nhtb9sq51r1?cid=storebadge&amp;ocid=badge" target="_blank">
+            <a class="m-auto block" href="//www.microsoft.com/store/apps/9nhtb9sq51r1?cid=storebadge&amp;ocid=badge" rel="nofollow" target="_blank">
               <img class="m-auto block" src="https://developer.microsoft.com/store/badges/images/English_get-it-from-MS.png" alt="English badge" style="height: 56px;">
             </a>
           </div>
@@ -251,12 +251,15 @@
                 </tr>
               </tbody>
             </table>
-            <div class="flex space-x-3">
-              <a href="https://snapcraft.io/antares" target="_blank">
-                <img alt="Get it from the Snap Store" src="https://snapcraft.io/static/images/badges/en/snap-store-black.svg">
+            <div class="flex flex-wrap justify-center gap-2">
+              <a href="https://flathub.org/apps/it.fabiodistasio.AntaresSQL" target="_blank" rel="nofollow">
+                <img class="inline-block h-14" alt="Download on Flathub" src="https://dl.flathub.org/assets/badges/flathub-badge-en.svg">
               </a>
-              <a href="https://aur.archlinux.org/packages/antares-sql-bin" target="_blank">
-                <img alt="Get it from AUR" src="https://raw.githubusercontent.com/antares-sql/antares/3e00c4bae6e036300c752c1a40c5a038fea9c169/docs/aur-badge.svg">
+              <a href="https://snapcraft.io/antares" target="_blank" rel="nofollow">
+                <img class="inline-block h-14" alt="Get it from the Snap Store" src="https://snapcraft.io/static/images/badges/en/snap-store-black.svg">
+              </a>
+              <a href="https://aur.archlinux.org/packages/antares-sql-bin" target="_blank" rel="nofollow">
+                <img class="inline-block h-14" alt="Get it from AUR" src="https://raw.githubusercontent.com/antares-sql/antares/master/docs/aur-badge.svg">
               </a>
             </div>
           </div>
